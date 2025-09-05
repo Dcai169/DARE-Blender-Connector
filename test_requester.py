19495f03d1cd4937dd4df6a5cfd351dd297857e4
Daniel Cai <17992507+Dcai169@users.noreply.github.com>
2025-09-05 01:08:45 -0400

    Update test_requester.py

--- test_requester.py
+++ test_requester.py
@@ -1,5 +1,5 @@
 import requests
-r = requests.get('http://localhost:41786', headers={'X-Content-Path': 'C:\\Users\\caid\\Documents\\DARE Output\\DestinyModel0'})
+r = requests.head('http://localhost:41786', headers={'X-Content-Path': 'C:\\Users\\caid\\Documents\\DARE Output\\DestinyModel0'})
 r.raise_for_status()
 
 print(r.status_code)
